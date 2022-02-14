72999646b7a01a689eab7e2ad5e7f8a763d74ebe
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-02-14 21:46:44 +0600

    Mail Improve

--- src/pages/api/mail.js
+++ src/pages/api/mail.js
@@ -1,21 +1,225 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const mail = require("@sendgrid/mail");
+const requestIp = require("request-ip");
 
 mail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default async function mailAPI(req, res) {
     const body = JSON.parse(req.body);
+    let detectedIp = ""
+    try {
+        detectedIp = requestIp.getClientIp(req)
+    }
+    catch (e){
+        detectedIp = "IP_ADDRESS_INVALID"
+    }
     const message = `
        Name: ${body.name}\r\n
        Email: ${body.email}\r\n
-       Message: ${body.message}
+       IP: ${detectedIp}\r\n
+       UserAgent: ${req.headers["user-agent"]}\r\n
+       Message: ${body.message}\r\n
     `;
     const data = {
         to: process.env.EMAIL,
         from: 'info@khanasfireza.dev',
         subject: "Message from your website - Khan Asfi Reza",
-        text: message,
-        html: message.replace(/\r\n/g, '<br>')
+        html: `
+        <!DOCTYPE html>
+
+<html lang="en" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:v="urn:schemas-microsoft-com:vml">
+<head>
+<title></title>
+<meta charset="utf-8"/>
+<meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+<!--[if mso]><xml><o:OfficeDocumentSettings><o:PixelsPerInch>96</o:PixelsPerInch><o:AllowPNG/></o:OfficeDocumentSettings></xml><![endif]-->
+<!--[if !mso]><!-->
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" type="text/css"/>
+<!--<![endif]-->
+<style>
+\t\t* {
+\t\t\tbox-sizing: border-box;
+\t\t}
+
+\t\tbody {
+\t\t\tmargin: 0;
+\t\t\tpadding: 0;
+\t\t}
+
+\t\ta[x-apple-data-detectors] {
+\t\t\tcolor: inherit !important;
+\t\t\ttext-decoration: inherit !important;
+\t\t}
+
+\t\t#MessageViewBody a {
+\t\t\tcolor: inherit;
+\t\t\ttext-decoration: none;
+\t\t}
+
+\t\tp {
+\t\t\tline-height: inherit
+\t\t}
+
+\t\t@media (max-width:520px) {
+\t\t\t.icons-inner {
+\t\t\t\ttext-align: center;
+\t\t\t}
+
+\t\t\t.icons-inner td {
+\t\t\t\tmargin: 0 auto;
+\t\t\t}
+
+\t\t\t.row-content {
+\t\t\t\twidth: 100% !important;
+\t\t\t}
+
+\t\t\t.stack .column {
+\t\t\t\twidth: 100%;
+\t\t\t\tdisplay: block;
+\t\t\t}
+\t\t}
+\t</style>
+</head>
+<body style="background-color: #FFFFFF; margin: 0; padding: 0; -webkit-text-size-adjust: none; text-size-adjust: none;">
+<table border="0" cellpadding="0" cellspacing="0" class="nl-container" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #FFFFFF;" width="100%">
+<tbody>
+<tr>
+<td>
+<table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-1" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #0068a5;" width="100%">
+<tbody>
+<tr>
+<td>
+<table align="center" border="0" cellpadding="0" cellspacing="0" class="row-content stack" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 500px;" width="500">
+<tbody>
+<tr>
+<td class="column" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 30px; padding-bottom: 30px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="100%">
+<table border="0" cellpadding="0" cellspacing="0" class="heading_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+<tr>
+<td style="width:100%;text-align:center;">
+<h1 style="margin: 0; color: #ffffff; font-size: 23px; font-family: 'Ubuntu', Tahoma, Verdana, Segoe, sans-serif; line-height: 120%; text-align: center; direction: ltr; font-weight: normal; letter-spacing: normal; margin-top: 0; margin-bottom: 0;"><strong>A new message from your website</strong></h1>
+</td>
+</tr>
+</table>
+<table border="0" cellpadding="10" cellspacing="0" class="text_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
+<tr>
+<td>
+<div style="font-family: Tahoma, Verdana, sans-serif">
+<div style="font-size: 14px; font-family: 'Source Sans Pro', Tahoma, Verdana, Segoe, sans-serif; mso-line-height-alt: 16.8px; color: #e8e8e8; line-height: 1.2;">
+</div>
+</div>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+<table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-2" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #def3ff;" width="100%">
+<tbody>
+<tr>
+<td>
+<table align="center" border="0" cellpadding="0" cellspacing="0" class="row-content stack" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 500px;" width="500">
+<tbody>
+<tr>
+<td class="column" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="100%">
+<table border="0" cellpadding="10" cellspacing="0" class="text_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
+<tr>
+<td>
+<div style="font-family: Tahoma, Verdana, sans-serif">
+<div style="font-size: 12px; font-family: 'Source Sans Pro', Tahoma, Verdana, Segoe, sans-serif; mso-line-height-alt: 14.399999999999999px; color: #555555; line-height: 1.2;">
+
+<p style="margin: 0; font-size: 18px;"><strong>IP: ${detectedIp}</strong></p>
+<p style="margin: 0; font-size: 18px;"><strong>User Agent: ${req.headers["user-agent"]}</strong></p>
+\t<br>
+<p style="margin: 0; font-size: 18px;"><strong>From ${body.name},</strong></p>
+<p style="margin: 0; font-size: 18px;"><strong>${body.email}</strong></p>
+\t<br>
+<p style="margin: 0; font-size: 18px;"><span style="font-size:15px;"><strong>${body.message}</strong></span></p>
+\t<br>
+\t<br>
+<p style="margin: 0; font-size: 18px;"><span style="font-size:15px;"><strong>To,</strong></span></p>
+<p style="margin: 0; font-size: 18px;"><span style="font-size:15px;"><strong>Khan Asfi Reza</strong></span></p>
+</div>
+</div>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+<table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-3" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+<tbody>
+<tr>
+<td>
+<table align="center" border="0" cellpadding="0" cellspacing="0" class="row-content stack" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 500px;" width="500">
+<tbody>
+<tr>
+<td class="column" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="100%">
+<table border="0" cellpadding="0" cellspacing="0" class="empty_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+<tr>
+<td>
+<div></div>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+<table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-4" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+<tbody>
+<tr>
+<td>
+<table align="center" border="0" cellpadding="0" cellspacing="0" class="row-content stack" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 500px;" width="500">
+<tbody>
+<tr>
+<td class="column" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="100%">
+<table border="0" cellpadding="0" cellspacing="0" class="icons_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+<tr>
+<td style="color:#9d9d9d;font-family:inherit;font-size:15px;padding-bottom:5px;padding-top:5px;text-align:center;">
+<table cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+<tr>
+<td style="text-align:center;">
+<!--[if vml]><table align="left" cellpadding="0" cellspacing="0" role="presentation" style="display:inline-block;padding-left:0px;padding-right:0px;mso-table-lspace: 0pt;mso-table-rspace: 0pt;"><![endif]-->
+<!--[if !vml]><!-->
+<table cellpadding="0" cellspacing="0" class="icons-inner" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; display: inline-block; margin-right: -4px; padding-left: 0px; padding-right: 0px;">
+<!--<![endif]-->
+
+</table>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table><!-- End -->
+</body>
+</html>
+`
     }
     await mail.send(data);
     await res.status(200).json({ message: 'Successfully sent the message' })

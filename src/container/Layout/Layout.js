4d489eb526da6ec20cbc81479751a9cfc2c1351a
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-03-05 01:09:28 +0600

    Layout Formatting Fix

--- src/container/Layout/Layout.js
+++ src/container/Layout/Layout.js
@@ -1,6 +1,6 @@
 const Layout = ({children}) => {
     return(
-        <main className={"bg-theme-light-200 dark:bg-theme-dark-200"}>
+        <main className={"bg-theme-light-200 dark:bg-theme-dark-200 overflow-x-hidden"}>
             {children}
         </main>
     )

79694ac8d0798bd3b13f9c64a13731c500f2993c
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-03-05 01:09:41 +0600

    Navigation System Changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,85 +1,126 @@
 import {useCustomTheme} from "../hooks/useCustomTheme";
 import classnames from "classnames";
-import {FaBars, FaTimes} from "react-icons/fa";
+import classNames from "classnames";
+import {FaBars, FaMoon, FaSun, FaTimes} from "react-icons/fa";
 import {useEffect, useRef, useState} from "react";
 import {ABOUT_ID, CONTACT_ID, HOME_ID, SERVICES_ID, SKILLS_ID} from "../assets/data/ContentID";
 import {gsap} from "gsap";
+import {Disclosure, Switch} from "@headlessui/react";
+
+const navigation = [
+    {name: 'Home', href: `#${HOME_ID}`, current: false},
+    {name: 'About', href: `#${ABOUT_ID}`, current: false},
+    {name: 'Skills', href: `#${SKILLS_ID}`, current: false},
+    {name: 'Services', href: `#${SERVICES_ID}`, current: false},
+    {name: 'Contact', href: `#${CONTACT_ID}`, current: false},
+]
 
 export default function Header() {
-
+    const [enabled, setEnabled] = useState(false)
     const {toggleTheme, getThemeIcon} = useCustomTheme();
-    const [navOpen, setNavOpen] = useState(false);
     const headerLinkRef = useRef(null);
 
-    useEffect(()=>{
-
-        for (let i =0; i < headerLinkRef.current.children.length; i++){
-
+    useEffect(() => {
+        for (let i = 0; i < headerLinkRef.current.children.length; i++) {
             gsap.from(headerLinkRef.current.children[i], {
-                opacity: 0,
-                y: -100,
+                opacity: 0, y: -100, delay: i * 0.2
             })
         }
     }, [])
 
-    const navOnClick = () => {
-        setNavOpen(!navOpen)
-    }
-
-    const navigationClassName = classnames(
-            "flex-auto md:px-6 " +
-        "list-none md:h-auto flex md:flex-row " +
-        "md:justify-around md:items-center md:static " +
-        "h-screen md:bg-transparent bg-primary text-white " +
-        "fixed top-0 flex-col transition-all duration-300 md:w-auto xs:w-3/6 w-3/4 md:py-0 py-3 px-5 z-100",
-        {
-            "-right-full": !navOpen,
-            "right-0": navOpen
-        }
 
 
-    )
-
-    return(
-        <header className={"m-auto fixed w-full top-0 left-0 h-16 bg-theme-light-100 dark:bg-theme-dark-100 grid place-items-center z-100 border-b border-zinc-200 dark:border-zinc-900"}>
-            <div className={"flex container m-auto px-2 py-3 h-full"}>
-                <div className={"flex-1 flex items-center"}>
-                    <h3 className={"dark:text-typo-dark-400 text-typo-light-400 text-xl md:text-2xl font-bold"}>Khan Asfi Reza</h3>
-                </div>
-                <div className={"flex-1 flex md:justify-start justify-end"}>
-                    <nav role={"navigation"}
-                         aria-label={"Navigation Menu"}
-                         className={navigationClassName}>
-                        <div className={"md:hidden flex justify-between items-center"}>
-                            <p className={"text-typo-dark-500 font-light text-3xl"}>
-                                Menu
-                            </p>
-
-                            <button onClick={navOnClick} aria-label={"navigation toggle"} className={"md:hidden grid place-items-center ml-3 text-primary bg-white text-center w-7 h-7 rounded-full"}>
-                                <FaTimes/>
-                            </button>
-
+    return (<Disclosure as="nav" className="fixed z-100 w-full dark:bg-theme-dark-100 bg-theme-light-100">
+            {({open}) => (
+                <>
+                    <div className="container mx-auto px-2 sm:px-6 lg:px-8">
+                        <div className="relative flex items-center justify-between h-16">
+                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                                {/* Mobile menu button*/}
+                                <Disclosure.Button
+                                    className="inline-flex items-center justify-center p-2 rounded-md dark:text-gray-300
+                                    text-gray-800 transition-all hover:text-white hover:bg-blue-500 focus:outline-none
+                                    focus:ring-2 focus:ring-inset focus:ring-white">
+                                    <span className="sr-only">Open main menu</span>
+                                    {open ? (
+                                        <div className="block h-6 w-6 grid place-items-center" aria-hidden="true">
+                                            <FaTimes/>
+                                        </div>
+                                    ) : (
+                                        <div className="block h-6 w-6 grid place-items-center" aria-hidden="true">
+                                            <FaBars/>
+                                        </div>
+                                    )}
+                                </Disclosure.Button>
+                            </div>
+                            <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                                <div className="flex-shrink-0 flex items-center">
+                                    <h2 className={"dark:text-white text-typo-light-400 font-bold text-xl"}>Khan Asfi
+                                        Reza</h2>
+                                </div>
+                                <div className="hidden sm:block sm:ml-6">
+                                    <div ref={headerLinkRef} className="flex space-x-4">
+                                        {navigation.map((item) => (
+                                            <a
+                                                key={item.name}
+                                                href={item.href}
+                                                className={classNames(
+                                                    'dark:text-gray-300 text-gray-800 hover:bg-blue-500 hover:dark:text-white hover:text-white',
+                                                    'px-3 py-2 rounded-md text-base font-medium'
+                                                )}
+                                                aria-current={item.current ? 'page' : undefined}
+                                            >
+                                                {item.name}
+                                            </a>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                            <div
+                                className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                                <Switch
+                                    checked={enabled}
+                                    onChange={() => {
+                                        setEnabled(!enabled);
+                                        toggleTheme()
+                                    }}
+                                    className={classNames(enabled ? 'bg-blue-500' : 'bg-sky-500',
+                                        "relative inline-flex flex-shrink-0 h-[38px] w-[74px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none ")}
+                                >
+                                    <span className="sr-only">Use setting</span>
+                                    <div
+                                        aria-hidden="true"
+                                        className={`${enabled ? 'translate-x-9' : 'translate-x-0'}
+                                pointer-events-none grid place-items-center h-[34px] w-[34px] rounded-full text-blue-500 bg-white shadow-lg transform ring-0 transition ease-in-out duration-200`}
+                                    >
+                                        {enabled ? <FaMoon/> : <FaSun/>}
+                                    </div>
+                                </Switch>
+                                {/* Profile dropdown */}
+                            </div>
                         </div>
-                        <hr className={"md:hidden text-typo-dark-300 w-full m-auto my-2"}/>
-                        <div ref={headerLinkRef} className={"md:flex-row md:h-auto h-3/4 flex flex-col justify-around"}>
-                            <a href={`#${HOME_ID}`} className="navbar-items text-lg dark:text-typo-dark-300 md:text-typo-light-300 text-typo-dark-300"> Home </a>
-                            <a href={`#${ABOUT_ID}`} className="navbar-items text-lg dark:text-typo-dark-300 md:text-typo-light-300 text-typo-dark-300"> About </a>
-                            <a href={`#${SKILLS_ID}`} className="navbar-items text-lg dark:text-typo-dark-300 md:text-typo-light-300 text-typo-dark-300"> Skills </a>
-                            <a href={`#${SERVICES_ID}`} className="navbar-items text-lg dark:text-typo-dark-300 md:text-typo-light-300 text-typo-dark-300"> Services </a>
-                            <a href={`#${CONTACT_ID}`} className="navbar-items text-lg dark:text-typo-dark-300 md:text-typo-light-300 text-typo-dark-300"> Contact </a>
-                        </div>
-                    </nav>
-                    <div className={"flex flex-row justify-end items-center"}>
-                        <button aria-label={"theme toggle"} onClick={toggleTheme}
-                                className={"grid place-items-center duration-500 bg-primary w-7 h-7 hover:transform hover:-translate-y-2 rounded-full transition-all  text-white "}>
-                            {getThemeIcon()}
-                        </button>
-                        <button onClick={navOnClick} aria-label={"navigation toggle"} className={"md:hidden ml-3 align-middle text-white bg-primary hover:bg-primaryLight text-center px-3 h-7 rounded-xl"}>
-                            <FaBars/>
-                        </button>
                     </div>
-                </div>
-            </div>
-        </header>
+
+                    <Disclosure.Panel className="sm:hidden">
+                        <div className="px-2 pt-2 pb-3 space-y-1">
+                            {navigation.map((item) => (
+                                <Disclosure.Button
+                                    key={item.name}
+                                    as="a"
+                                    href={item.href}
+                                    className={classNames(
+                                        'dark:text-gray-300 text-gray-800 hover:bg-primary hover:text-white',
+                                        'block px-3 py-2 rounded-md text-base font-medium'
+                                    )}
+                                    aria-current={item.current ? 'page' : undefined}
+                                >
+                                    {item.name}
+                                </Disclosure.Button>
+                            ))}
+                        </div>
+                    </Disclosure.Panel>
+                </>
+            )}
+        </Disclosure>
     )
 }

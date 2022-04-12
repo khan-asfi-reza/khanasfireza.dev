e6ac32fc51ac984c4fb29c2ca447d4d24cca6748
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-04-12 15:55:41 +0600

    GSAP Removed Framer motion added

--- src/elements/Home/Skills.js
+++ src/elements/Home/Skills.js
@@ -1,13 +1,12 @@
 import {SectionContainer} from "../../components/SectionContainer";
 import {SectionHeader} from "../../components/SectionHeader";
 import {SectionRow} from "../../components/SectionRow";
-import {gsap} from "gsap";
 import {SKILLS_ID} from "../../assets/data/ContentID";
 import {AllSkillContents} from "../../assets/data/Content";
 import {useEffect, useRef, useState} from "react";
 import classNames from "classnames";
-import Carousel, {CarouselSlide} from "../../components/Carousel";
-
+import {AnimatePresence, motion} from "framer-motion";
+import {moveFromLeft} from "../../const";
 
 const WorkingExperience = [
     {
@@ -33,92 +32,85 @@ const WorkingExperience = [
 ]
 
 export const Skills = () => {
-   const [current, setCurrent] = useState(0);
+    const [current, setCurrent] = useState(0);
 
-   const workingRef = useRef(null);
+    const workingRef = useRef(null);
 
 
-   useEffect(()=>{
-       let sl = gsap.timeline({
-           scrollTrigger: {
-               trigger: "#"+SKILLS_ID,
-           }
-       });
-       sl.from("#skill_text", {
-           opacity: 0,
-           x: -100,
-       })
+    useEffect(() => {
+        let timeOut = setTimeout(() => {
+            if (current === 0) {
+                setCurrent(1);
+            } else {
+                setCurrent(0);
+            }
+        }, 5000)
+        return () => {
+            clearTimeout(timeOut)
+        }
+    }, [current])
 
-       sl.from("#SKILL_CAROUSEL", {
-           opacity: 0,
-           x: -100
-       })
+    return (
+        <SectionContainer id={SKILLS_ID}>
+            <SectionRow className={"gap-x-24 gap-y-20 mt-12"}>
+                <div className={"flex flex-col justify-between"}>
+                    <div id={"skill_text"}>
+                        <SectionHeader header={"Skills and Experience"} headerClassName={"text-left text-primary"}/>
+                        <p className={"mt-14 dark:text-typo-dark-100 text-typo-light-200"}>
+                            I have professional experience in more than 10+ Languages and Frameworks along with
+                            relational databases and many other
+                            technical tools. I have worked with 3 Companies and several clients to provide services.
+                        </p>
+                    </div>
+                    <div id={"SKILL_CAROUSEL"} className={"mt-12"}>
+                        <h2 className={"dark:text-typo-dark-400 text-typo-light-400 text-4xl font-semibold"}>Skills</h2>
+                        <div className={"flex flex-row overflow-hidden"}>
+                            <AnimatePresence exitBeforeEnter={true}>
+                            {AllSkillContents.map((each, key) => (
+                                current === key &&
+                                <motion.div {...moveFromLeft()} exit={{x: 100, opacity: 0}} className={"w-full mx-auto flex-full"} key={key}>
+                                    <div className={"grid grid-cols-3 md:gap-10 gap-3 gap-y-10 mt-12"}>
+                                        {each.map((content, k) => (
+                                            <div key={k} id={`skill_content_${key}`}
+                                                 className={classNames("transition-all")}>
+                                                <div className={"md:h-20 h-16 relative"}>
+                                                    <img className={"md:h-20 h-16 "} src={content.logo.src}
+                                                         alt={content.name}/>
+                                                </div>
+                                                <p className={"text-base  dark:text-typo-dark-100 text-typo-light-200 mt-5"}>
+                                                    {content.name}
+                                                </p>
+                                            </div>
+                                        ))}
+                                    </div>
+                                </motion.div>
+                            ))}
+                            </AnimatePresence>
+                        </div>
+                    </div>
+                </div>
 
-       Array.from(workingRef.current.children).map(each => {
-           sl.from(each, {
-               opacity: 0,
-               y: -100,
-               duration: 0.5
-           }, )
-       })
-   }, [])
+                <div className={"flex flex-col justify-between pt-20"}>
+                    <h3 className={"dark:text-typo-dark-400 text-typo-light-400 text-4xl font-semibold"}>Experience</h3>
+                    <div ref={workingRef} className={"mt-20"}>
+                        {
+                            WorkingExperience.map(
+                                (each, k) => (
+                                    <div key={k}
+                                         className={"py-4 grid grid-cols-12 border-b border-0.5 dark:border-typo-light-300 border-typo-dark-300 mt-5"}>
+                                        <p className={"md:col-span-3 col-span-4 dark:text-typo-dark-100 text-typo-light-100 mr-6"}>{each.validity}</p>
+                                        <div className={"md:col-span-9 col-span-8 flex flex-col"}>
+                                            <h2 className={"md:text-2xl text-xl font-medium dark:text-typo-dark-200 text-typo-light-200"}>{each.title}</h2>
+                                            <p className={"mt-3 md:text-base text-sm dark:text-typo-dark-100 text-typo-light-100"}>{each.workplace}</p>
+                                        </div>
+                                    </div>
+                                )
+                            )
+                        }
+                    </div>
+                </div>
+            </SectionRow>
+        </SectionContainer>
 
-  return(
-      <SectionContainer id={SKILLS_ID}>
-          <SectionRow className={"gap-x-24 gap-y-20 mt-12"}>
-              <div className={"flex flex-col justify-between"}>
-                  <div id={"skill_text"}>
-                      <SectionHeader header={"Skills and Experience"} headerClassName={"text-left text-primary"}/>
-                      <p className={"mt-14 dark:text-typo-dark-100 text-typo-light-200"}>
-                          I have professional experience in more than 10+ Languages and Frameworks along with relational databases and many other
-                          technical tools. I have worked with 3 Companies and several clients to provide services.
-                      </p>
-                  </div>
-                  <div id={"SKILL_CAROUSEL"} className={"mt-12"}>
-                      <h2 className={"dark:text-typo-dark-400 text-typo-light-400 text-4xl font-semibold"}>Skills</h2>
-                      <Carousel autoplay controls={false} navigation={false}>
-                          {AllSkillContents.map((each, key) => (
-                              <CarouselSlide className={"w-full mx-auto"} key={key}>
-                                      <div  className={"grid grid-cols-3 md:gap-10 gap-3 gap-y-10 mt-12"}>
-                                          {each.map((content, k) => (
-                                          <div key={k} id={`skill_content_${key}`} className={classNames("transition-all")}>
-                                              <div className={"md:h-20 h-16"}>
-                                                  <img className={"md:h-20 h-16"} src={content.logo.src} alt={content.name}/>
-                                              </div>
-                                              <p className={"text-base dark:text-typo-dark-100 text-typo-light-200 mt-5"}>
-                                                  {content.name}
-                                              </p>
-                                          </div>
-                                          ))}
-                                      </div>
-
-                              </CarouselSlide>
-                          ))}
-                      </Carousel>
-                  </div>
-              </div>
-
-              <div className={"flex flex-col justify-between pt-20"}>
-                  <h3 className={"dark:text-typo-dark-400 text-typo-light-400 text-4xl font-semibold"}>Experience</h3>
-                  <div ref={workingRef} className={"mt-20"}>
-                      {
-                          WorkingExperience.map(
-
-                              (each, k) => (
-                                  <div key={k} className={"py-4 grid grid-cols-12 border-b border-0.5 dark:border-typo-light-300 border-typo-dark-300 mt-5"}>
-                                      <p className={"md:col-span-3 col-span-4 dark:text-typo-dark-100 text-typo-light-100 mr-6"}>{each.validity}</p>
-                                      <div className={"md:col-span-9 col-span-8 flex flex-col"}>
-                                          <h2 className={"md:text-2xl text-xl font-medium dark:text-typo-dark-200 text-typo-light-200"}>{each.title}</h2>
-                                          <p className={"mt-3 md:text-base text-sm dark:text-typo-dark-100 text-typo-light-100"}>{each.workplace}</p>
-                                      </div>
-                                  </div>
-                              )
-                          )
-                      }
-                  </div>
-              </div>
-          </SectionRow>
-      </SectionContainer>
-
-  )
+    )
 }
